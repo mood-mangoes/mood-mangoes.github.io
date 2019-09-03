9d5a9afb77575e84e8e2fd01fc9296a8146f12ba
evanandrewson <evanandrewson@gmail.com>
2019-09-03 09:27:30 -0700

    started API call

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
+// app.post('/v3/tone', (request, response) => {
+//     client.query(`
+//     INSERT INTO text (user_id, name, category, body)
+//                     VALUES ($1, $2, $3, $4)
+//                     RETURNING *;
+//                 `,
+//                 [, , , ])
+//     `)
+// });
+
 app.listen(PORT, () => {
     console.log('server running on port', PORT);
 });
