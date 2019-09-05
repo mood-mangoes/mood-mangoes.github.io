c43f870afc2ac4d74452308503638a6b16890f88
Abbey Masters <abbeykmasters@gmail.com>
2019-09-05 10:02:13 -0700

    fixing tooltip on hover

--- public/src/components/message-tester/SentenceResults.js
+++ public/src/components/message-tester/SentenceResults.js
@@ -10,39 +10,46 @@ class SentenceResults extends Component {
             sentenceResultsData.forEach(line => {
                 if(sentence.textContent === line.sentence) {
                     toneArray.push(line.tone_id);
-                    // const span = document.createElement('span');
-                    // span.classList.add(line.tone_id);
-                    // span.classList.add('dot');
-                    // sentence.prepend(span);
-                
-                    
+                    toneArray.push(line.score);
                 }
-                
             });
-            if(toneArray.length === 1) {
-                const hr = document.createElement('hr');
-                hr.classList.add('first-hr');
-                hr.classList.add(toneArray[0]);
-                hr.classList.add('tooltip');
-                sentence.appendChild(hr);
-                const toolTip = document.createElement('span');
-                toolTip.classList.add('tooltip-text');
-                toolTip.textContent = 'toneArray[0]: ';
-                hr.appendChild(toolTip);
-                
-            }
             if(toneArray.length === 2) {
                 const hr = document.createElement('hr');
                 hr.classList.add('first-hr');
                 hr.classList.add(toneArray[0]);
                 hr.classList.add('tooltip');
                 sentence.appendChild(hr);
+                const toolTip = document.createElement('span');
+                let score = +toneArray[1];
+                score = score.toFixed(2);
+                toolTip.classList.add('tooltip-text');
+                toolTip.textContent = `${toneArray[0]}: ${score}`;
+                hr.appendChild(toolTip);
+            }
+            if(toneArray.length === 4) {
+                const hr = document.createElement('hr');
+                hr.classList.add('first-hr');
+                hr.classList.add(toneArray[0]);
+                hr.classList.add('tooltip');
+                sentence.appendChild(hr);
+                const toolTip = document.createElement('span');
+                let score = +toneArray[1];
+                score = score.toFixed(2);
+                toolTip.classList.add('tooltip-text');
+                toolTip.textContent = `${toneArray[0]}: ${score}`;
+                hr.appendChild(toolTip);
                 
                 const hrTwo = document.createElement('hr');
                 hrTwo.classList.add('second-hr');
-                hrTwo.classList.add(toneArray[1]);
-                hr.classList.add('tooltip');
+                hrTwo.classList.add(toneArray[2]);
+                hrTwo.classList.add('tooltip');
                 sentence.appendChild(hrTwo);
+                const toolTipTwo = document.createElement('span');
+                let scoreTwo = +toneArray[3];
+                scoreTwo = scoreTwo.toFixed(2);
+                toolTipTwo.classList.add('tooltip-text');
+                toolTipTwo.textContent = `${toneArray[2]}: ${scoreTwo}`;
+                hrTwo.appendChild(toolTipTwo);
             }
         });
 
